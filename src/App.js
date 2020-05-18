ce1de86469e9da66e3c1fb9490d74af57a189c74
Thomas Smith (Tom) <tomytank@users.noreply.github.com>
2020-05-18 15:59:27 +0200

    Setting up
    Creating files, defining components and linking props.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import "./styles.css";
+
+import Users from "./components/Users";
 
 export default function App() {
   return (
     <div className="App">
+      <Router path="/contact" component={Contact} />
+      <Router exact path="/" component={Home} />
+      <Router path="/about" component={About} />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
---
+++ src/components/Users.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "react-dom";
+
+
+const users = () => {
+
+
+
+  render() {
+  return (
+  <div>
+  Test
+  </div>
+);
+
+}
+
+}
+
+
+export default Users;
