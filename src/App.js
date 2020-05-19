f949d64f1e667f36c881e8ed0c8d777586fc6ee2
Thomas Smith (Tom) <tomytank@users.noreply.github.com>
2020-05-19 10:31:33 +0200

    Added Nav bar
    Nav bar, basic landing pages, Links added to document.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+//import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import "./styles.css";
 
-import Users from "./components/Users";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+console.log(<Home />);
+import About from "./components/About";
+import AvengersList from "./components/AvengersList";
+//import Users from "./components/Users";
 
 export default function App() {
   return (
-    <div className="App">
-      <Router path="/contact" component={Contact} />
-      <Router exact path="/" component={Home} />
-      <Router path="/about" component={About} />
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <ul className="navbar">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/avengers">Avengers</Link>
+          </li>
+        </ul>
+        {/* <Router path="/contact" component={Contact} /> */}
+        Goodness Gracious
+        <Route exact path="/" component={Home} />
+        <Route path="/avengers" component={AvengersList} />
+      </div>
+    </Router>
   );
 }
